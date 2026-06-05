59bdc2b23bc16c2c589173c2c7569d4cb59159b2
Juan Diego Rodriguez <rodriguezsalasjuandiego070@gmail.com>
2026-06-04 21:22:30 -0400

    feat: elimine la carpeta auth

--- app/auth/login/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Login() {
-    return (
-        <div className="flex items-center justify-center min-h-screen">
-            <h1 className="text-4xl font-bold">hola mundo</h1>
-        </div>
-    )
-}
