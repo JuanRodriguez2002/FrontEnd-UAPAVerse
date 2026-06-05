36e0579f0dede27e99b7f85319de3c8620d7fb19
Juan Diego Rodriguez <rodriguezsalasjuandiego070@gmail.com>
2026-06-05 00:21:42 -0400

    feat: ajustes de carpetas y trabajo

---
+++ features/auth/components/ConfirmPasswordForm.tsx
@@ -0,0 +1,3 @@
+export function ConfirmPasswordForm() {
+  return <div className="p-4 border rounded"> [Aquí el Desarrollador A programará el formulario de confirmación de contraseña] </div>;
+}
---
+++ features/auth/components/RegisterForm.tsx
@@ -0,0 +1,3 @@
+export function RegisterForm() {
+  return <div className="p-4 border rounded"> [Aquí el Desarrollador A programará el formulario de register] </div>;
+}
