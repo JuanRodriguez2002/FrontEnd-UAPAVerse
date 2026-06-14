840db5a06a2ded314f22cea68fed1fadfb244856
OliverRD <Oliverramos1406@gmail.com>
2026-06-13 23:57:17 -0400

    feat: stands y agregado para expositores

---
+++ app/dashboard-expositor/panelEstadisticas/page.tsx
@@ -0,0 +1,5 @@
+import PanelEstadisticas from "@/features/dashboard-presentador/components/PanelEstadisticas";
+
+export default function panelEstadisticasPage() {
+  return <PanelEstadisticas />;
+}
