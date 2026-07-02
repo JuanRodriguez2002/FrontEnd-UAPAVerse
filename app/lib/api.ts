24dd2817b2f34284fca4b5276ad6a35559da1fe2
OliverRD <Oliverramos1406@gmail.com>
2026-07-02 16:11:22 -0400

    usar fetch

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,26 +1,28 @@
-import axios from "axios";
+// import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://backend-uapaverse.onrender.com/api", // Tu URL base real
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const api = axios.create({
+//   baseURL: "https://backend-uapaverse.onrender.com/api", // Tu URL base real
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-// Interceptor esencial para evitar el error 401 Unauthorized
-api.interceptors.request.use(
-  (config) => {
-    if (typeof window !== "undefined") {
-      const token = localStorage.getItem("token"); // Lee la key "token"
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`; // Inserta el Bearer token
-      }
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// // Interceptor esencial para evitar el error 401 Unauthorized
+// api.interceptors.request.use(
+//   (config) => {
+//     if (typeof window !== "undefined") {
+//       const token = localStorage.getItem("token"); // Lee la key "token"
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`; // Inserta el Bearer token
+//       }
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
-export default api;
+// export default api;
+
+//usar fetch en lugar de axios
